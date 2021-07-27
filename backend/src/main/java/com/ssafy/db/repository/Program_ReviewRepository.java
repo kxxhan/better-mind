3b72a3e5af18793b26bd8f532611eb62d7d1e8b0
DESKTOP-KVCQHCD\multicampus <23gywls@naver.com>
2021-07-27 10:50:43 +0900

    #S05P12B208-216 feat: 프로그램, 후기, 전문가 디비 생성

---
+++ backend/src/main/java/com/ssafy/db/repository/Program_ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.ssafy.db.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.db.entity.Program_Review;
+
+public interface Program_ReviewRepository extends JpaRepository<Program_Review, Long> {
+
+}
+
