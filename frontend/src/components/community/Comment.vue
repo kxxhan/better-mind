357b3ca5889df113197ce8fab6dc992478c6db29
whgywjd123 <whgywjd6794@gmail.com>
2021-07-29 16:44:59 +0900

    #S05P12B208-206 feat: 댓글 컴포넌트 구현

--- frontend/src/components/community/Comment.vue
+++ frontend/src/components/community/Comment.vue
@@ -1,15 +1,29 @@
 <template>
-  <div id="comment">
-    <ul>
-      <li>댓글 1</li>
-      <li>댓글 2</li>
-    </ul>
-  </div>
+  <v-container id="comment">
+    <v-list disabled>
+      <v-subheader>Comment List</v-subheader>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+            <hr>
+          </v-list-item-content>
+        </v-list-item>
+    </v-list>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+      items: [
+        { text: '댓글1'},
+        { text: '댓글2'},
+        { text: '댓글3'},
+      ],
+    }),
 }
 </script>
 
