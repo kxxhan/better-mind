dbd4eea6ac09ab57f67efc2651d0466c99036817
whgywjd123 <whgywjd6794@gmail.com>
2021-07-29 15:53:42 +0900

    #S05P12B208-209 feat: 게시글 세부정보 및 댓글작성 폼 구현

--- frontend/src/views/community/PostDetail.vue
+++ frontend/src/views/community/PostDetail.vue
@@ -1,16 +1,60 @@
 <template>
-  <div id="post-detail">
-    <h3>제목</h3>
-    <h4>내용</h4>
-    <button @click="updatePost()">수정</button>
-    <button>삭제</button>
-    <div>
-      <label for="comment">댓글 작성</label>
-      <input type="text" id="comment" name="comment" required>
-      <button>작성</button>
-      <Comment/>
-    </div>
-  </div>
+  <v-container id="post-detail">
+    <!-- 게시글 상세내용 -->
+    <v-card
+      class="mx-auto mb-5"
+    >
+      <v-container id="post">
+        <v-card-title>
+          Article title
+        </v-card-title>
+
+        <v-card-subtitle>
+          Subtitle text
+        </v-card-subtitle>
+
+        <v-card-text>
+          Greyhound divisively hello coldly wonderfully marginally far upon excluding.
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn icon class="mx-2">
+            <v-icon>mdi-heart</v-icon>
+            <span>3</span>
+          </v-btn>
+          <span>#해시태그</span>
+        </v-card-actions>
+        
+        <v-card-actions>
+          <v-btn @click="updatePost()">수정</v-btn>
+          <v-btn>삭제</v-btn>
+        </v-card-actions>
+      </v-container>
+    </v-card>
+    
+    <!-- 댓글작성 폼 -->
+    <v-row class="my-5 d-flex align-center">
+      <v-textarea
+        prepend-inner-icon="mdi-comment"
+        class="mx-2"
+        label="comment"
+        rows="1"
+        cols="12"
+        v-model="commentItem"
+      ></v-textarea>
+      <!-- <input type="text" id="comment" name="comment" required> -->
+      <v-btn 
+        cols="1"
+        @click="createComment()"
+      >
+        작성
+      </v-btn>
+    </v-row>
+
+    <!-- 댓글 리스트 -->
+    <Comment/>
+  </v-container>
 </template>
 
 <script>
@@ -18,12 +62,20 @@ import Comment from '@/components/community/Comment.vue'
 
 export default {
   name: 'PostDetail',
+  data: function () {
+    return {
+      commentItem: '',
+    }
+  },
   components: {
     Comment,
   },
   methods: {
     updatePost: function () {
       this.$router.push({ name: 'UpdatePost' })
+    },
+    createComment: function () {
+      console.log(this.commentItem)
     }
   }
 }
