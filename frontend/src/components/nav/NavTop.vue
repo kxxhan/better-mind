0b37d1cb3f299f3ad23dead50cfdc05e3bf46fd5
오승철 <tmdcjf103@gmail.com>
2021-07-29 16:33:16 +0900

    #S05P12B208-128 feat: 로그인 Dialog 구현

--- frontend/src/components/nav/NavTop.vue
+++ frontend/src/components/nav/NavTop.vue
@@ -7,7 +7,7 @@
     <router-link :to="{ name: 'Main' }" class="navlink mx-3">로고</router-link>
     <div class="ms-auto">
       <router-link :to="{ name: 'Signup' }" class="navlink mx-3">회원가입</router-link>
-      <span class="navlink mx-3">로그인</span>
+      <LoginModal/>
       <span class="navlink mx-3">로그아웃</span>
       <router-link :to="{ name: 'MyPage' }" class="navlink mx-3">마이 페이지</router-link>
     </div>
@@ -15,8 +15,15 @@
 </template>
 
 <script>
+import LoginModal from '../LoginModal.vue'
+
 export default {
   name: 'NavTop',
+  components: {
+    LoginModal
+  },
+  methods: {
+  }
 }
 </script>
 
