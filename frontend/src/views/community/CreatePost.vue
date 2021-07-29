916c7a3d8cec8655cd450ffeb4551beaa95cccfb
whgywjd123 <whgywjd6794@gmail.com>
2021-07-29 14:29:18 +0900

    #S05P12B208-209 feat: 게시글 생성버튼 및 작성폼 구현

--- frontend/src/views/community/CreatePost.vue
+++ frontend/src/views/community/CreatePost.vue
@@ -1,13 +1,113 @@
 <template>
-  <div id="create-post">
-    <h3>새 글 작성</h3>
-    <p>입력 폼 비워서 제공</p>
-  </div>
+  <v-container id="create-post">
+    <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+    >
+      <div class="text-h6">게시글 작성</div>
+      <v-text-field
+        v-model="title"
+        :counter="50"
+        :rules="titleRules"
+        label="Title"
+        required
+      ></v-text-field>
+
+      <v-textarea
+        outlined
+        v-model="content"
+        :rules="contentRules"
+        label="Content"
+        required
+        height="350px"
+      ></v-textarea>
+
+      <v-select
+        v-model="select"
+        :items="category"
+        :rules="[v => !!v || 'Category is required']"
+        label="Category"
+        required
+      ></v-select>
+
+      <v-text-field
+        v-model="hashtag"
+        :rules="hashtagRules"
+        label="Hashtag"
+        required
+      ></v-text-field>
+
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="submit"
+      >
+        Submit
+      </v-btn>
+
+      <!-- <v-btn
+        color="error"
+        class="mr-4"
+        @click="reset"
+      >
+        Reset Form
+      </v-btn> -->
+
+      <v-btn
+        color="warning"
+        @click="movePostItems"
+      >
+        Back
+      </v-btn>
+    </v-form>
+  </v-container>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    valid: true,
+    title: '',
+    titleRules: [
+      v => !!v || 'Title is required',
+      v => (v && v.length <= 50) || 'Title must be less than 50 characters',
+    ],
+    content: '',
+    contentRules: [
+      v => !!v || 'Content is required',
+      v => (v && v.length > 0) || 'Content must be valid',
+    ],
+    select: null,
+    category: [
+      '학교',
+      '직장',
+      '대인 관계',
+      '가족',
+      '외모',
+      '등'
+    ],
+    hashtag: '',
+    hashtagRules: [
+      v => !!v || 'Hashtag is required',
+      v => (v && v.length > 0) || 'Hashtag must be valid',
+    ],
+  }),
 
+  methods: {
+    submit () {
+      if (this.$refs.form.validate()) {
+        console.log('submit')
+      }
+    },
+    // reset () {
+    //   this.$refs.form.reset()
+    // },
+    movePostItems () {
+      this.$router.push({ name: 'PostItems' })
+    },
+  },
 }
 </script>
 
