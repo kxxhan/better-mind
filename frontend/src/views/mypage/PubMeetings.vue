f394cdad4a2af99026fafeca5aee99a846a9fbf1
whgywjd123 <whgywjd6794@gmail.com>
2021-08-03 10:30:26 +0900

    feat: 마이페이지-개인 미팅정보목록 구현

--- frontend/src/views/mypage/PubMeetings.vue
+++ frontend/src/views/mypage/PubMeetings.vue
@@ -1,7 +1,42 @@
 <template>
   <v-container id="pub-meetings">
     <NavSide/>
-    일반 사용자의 미팅 정보
+    <v-item-group>
+      <v-row>
+        <v-col
+          v-for="n in 6"
+          :key="n"
+          cols="12"
+          sm="6"
+          md="4"
+        >
+          <v-item class="d-flex flex-column justify-center mb-2">
+            <v-card
+              class="mx-auto"
+              max-width="350"
+              @click="showDetail()"
+            >
+              <v-img
+                class="white--text align-end"
+                height="250px"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+              >
+                <v-card-title>프로그램 이미지</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pb-0 text-subtitle-1">
+                프로그램 이름
+              </v-card-subtitle>
+
+              <v-card-text class="text--primary">
+                <div>미팅주소</div>
+                <div>미팅시간</div>
+              </v-card-text>
+            </v-card>
+          </v-item>
+        </v-col>
+      </v-row>
+    </v-item-group>
   </v-container>
 </template>
 
