e660d7f7d29b8d845b34e0680f27ba6f45bdded0
whgywjd123 <whgywjd6794@gmail.com>
2021-08-04 11:53:18 +0900

    #S05P13B208-91 feat: 전문가 프로그램 등록 API 연동

--- frontend/src/views/mypage/ExpAddMeeting.vue
+++ frontend/src/views/mypage/ExpAddMeeting.vue
@@ -2,46 +2,57 @@
   <v-container id="exp-add-meeting">
     전문가 프로그램 등록 페이지 | 
     프로그램명, 전문가소개 => user정보에서 가져오기, 프로그램소개, 카테고리설정, 기간, 시간, 진행차시, 비용
+    
     <v-form
       ref="form"
       v-model="valid"
       lazy-validation
     >
       <v-text-field
-        v-model="program.title"
-        :counter="10"
-        :rules="titleRules"
-        label="Title"
+        v-model="program.name"
+        :rules="nameRules"
+        label="Name"
         required
       ></v-text-field>
 
       <v-text-field
-        v-model="program.content"
-        :rules="contentRules"
-        label="Content"
+        v-model="program.report"
+        :rules="reportRules"
+        label="Program Content"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="program.description"
+        :rules="descriptionRules"
+        label="Expert Description"
         required
       ></v-text-field>
 
       <!-- 가능하면 캘린더 사용 -->
       <v-text-field
-        v-model="program.period"
-        :counter="10"
-        :rules="periodRules"
+        v-model="program.date"
+        :rules="dateRules"
         label="2021-01-01/2021-12-31"
         required
       ></v-text-field> 
 
       <v-text-field
         v-model="program.time"
-        :counter="10"
         :rules="timeRules"
         label="월, 목/19:30~21:00"
+        required
+      ></v-text-field>  
+
+      <v-text-field
+        v-model="program.count"
+        :rules="countRules"
+        label="Number of participants"
         required
       ></v-text-field>   
       
       <v-text-field
         v-model="program.price"
-        :counter="10"
         :rules="priceRules"
         label="Price"
         required
@@ -56,21 +67,15 @@
           active-class="primary--text"
         >
           <v-chip
-            v-for="item in categories"
+            v-for="item in category"
             :key="item"
+            @click="choiceCategory(item)"
           >
             {{ item }}
           </v-chip>
         </v-chip-group>
       </v-sheet> 
       
-      <v-checkbox
-        v-model="program.checkbox"
-        :rules="[v => !!v || 'You must agree to continue!']"
-        label="Do you agree?"
-        required
-      ></v-checkbox>
-
       <v-btn
         :disabled="!valid"
         color="success"
@@ -92,38 +97,49 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'ExpAddMeeting',
   data: () => ({
     valid: true,
-    categories: ['우울', '불안', '성격문제', '애착', 
+    category: ['우울', '불안', '성격문제', '애착', 
           '대인기피', '분노조절', '자존감', 
           '중독', '대인관계', '부부', '가족상담',
         ],
     program: {
-      title: '',
-      content: '',
-      period: '',
+      name: '',
+      report: '',
+      description: '',
+      date: '',
       time: '',
       price: '',
-      categories: '우울',
-      checkbox: false,
+      category: '',
+      userId: "ssafy_web"
     },
-    titleRules: [
+    nameRules: [
       v => !!v || 'Name is required',
-      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      v => v.length > 0 || 'Name must be more than 0 characters',
     ],
-    contentRules: [
-      v => !!v || 'Content is required',
-      v =>  v.length > 10 || 'Content must be more than 10 characters',
+    reportRules: [
+      v => !!v || 'Program content is required',
+      v =>  v.length > 10 || 'Program content must be more than 10 characters',
     ],
-    periodRules: [
-      v => !!v || 'Period is required',
-      v =>  v.length == 21 || 'Period must be 21 characters ex)2021-01-01/2021-12-31',
+    descriptionRules: [
+      v => !!v || 'Expert description is required',
+      v =>  v.length > 10 || 'Expert description must be more than 10 characters',
+    ],
+    dateRules: [
+      v => !!v || 'Date is required',
+      v =>  v.length == 21 || 'Date must be 21 characters ex)2021-01-01/2021-12-31',
     ],
     timeRules: [
       v => !!v || 'Time is required',
       v =>  v.length > 0 || 'Time must be 21 characters ex)월, 목/19:30~21:00',
+    ],
+    countRules: [
+      v => !!v || 'Number of participants is required',
+      v =>  v.length > 0 || 'Number of participants must be more than 0',
     ],
     priceRules: [
       v => !!v || 'Period is required',
@@ -134,12 +150,25 @@ export default {
   methods: {
     addProgram: function () {
       if (this.$refs.form.validate()) {
-        console.log(this.program)
+        axios({
+          method: 'post',
+          url: '/api/v1/program',
+          data: this.program
+        })
+        .then (() => {
+          this.$router.push({ name: 'ExpMeetings' })
+        })
+        .catch((err) => {
+          console.log(err)
+        })
       }
     },
-    reset () {
-      this.$refs.form.reset()
-    },
+    choiceCategory: function (item) {
+      this.program.category = item
+    }
+    // reset () {
+    //   this.$refs.form.reset()
+    // },
   },
 }
 </script>
