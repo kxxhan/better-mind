80d3ff234ab0ecc09e079ffe833c7cc1c3efff77
오승철 <tmdcjf103@gmail.com>
2021-08-04 15:22:16 +0900

    #S05P13B208-77 feat: 댓글 API 삭제 요청

--- frontend/src/components/community/Comment.vue
+++ frontend/src/components/community/Comment.vue
@@ -5,8 +5,8 @@
         <v-divider></v-divider>
         <v-container>
           <v-list-item
-            v-for="(comment, i) in comments"
-            :key="i"
+            v-for="comment in comments"
+            :key="comment.id"
           >
             
             <v-list-item-content>
@@ -14,8 +14,7 @@
               <v-list-item-title v-text="comment.content"></v-list-item-title>
             </v-list-item-content>
             <v-list-item-action v-show="comment.userId === userInfo.id">
-              <!-- 삭제하려면 comment id가 필요하지만 알 방법이 없음 -->
-              <v-btn icon>
+              <v-btn icon @click="deleteComment(comment)">
                 <v-icon>mdi-window-close</v-icon>
               </v-btn>
             </v-list-item-action>
@@ -26,10 +25,24 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Comment',
   methods: {
-
+    // 댓글 삭제 Axios 및 삭제 후 댓글 컴포넌트 reload를 위한 emit 전달
+    deleteComment: function (comment) {
+      axios({
+        method: 'delete',
+        url: `/api/v1/article/${this.$route.params.post_pk}/${comment.id}`,
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('jwt')}`
+        }
+      })
+      .then(() => {
+        this.$emit('render')
+      })
+    }
   },
   computed: {
     comments: function () {
