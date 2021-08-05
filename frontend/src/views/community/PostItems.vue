1fb0620bffd288f413e6c353a810d49c06fe30ee
whgywjd123 <whgywjd6794@gmail.com>
2021-08-05 23:20:30 +0900

    #S05P13B208-80 feat: 게시글 카테고리 필터기능 구현

--- frontend/src/views/community/PostItems.vue
+++ frontend/src/views/community/PostItems.vue
@@ -19,10 +19,11 @@
               multiple
             >
               <v-chip
-              v-for="tag in tags"
-              :key="tag"
+                v-for="item in category"
+                :key="item"
+                @click="selectCategory(item)"
               >
-                {{ tag }}
+                {{ item }}
               </v-chip>
             </v-chip-group>
           </div>
@@ -37,7 +38,52 @@
     <!-- 게시글 리스트 -->
     <v-item-group>
       <v-container>
-        <v-row>
+        <!-- 선택 카테고리에 대한 게시글 렌더링 -->
+        <v-row v-if="selectCategories.length">
+          <v-col
+            v-for="post in selectPosts"
+            :key="post.id"
+            cols="12"
+            md="6"
+          >
+            <v-item>
+              <v-card
+                class="d-flex flex-column justify-center"
+                height="250"
+                @click="showDetail(post)"
+              >
+                <v-card-title>
+                  {{ post.title }}
+                </v-card-title>
+
+                <v-card-subtitle>
+                  {{ post.category }}
+                </v-card-subtitle>
+
+                <v-card-text>
+                  {{ post.content }}
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+
+                  <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                    <span>-</span>
+                  </v-btn>
+
+                  <v-btn icon>
+                    <v-icon>mdi-message-text</v-icon>
+                    <span>-</span>
+                  </v-btn>
+
+                </v-card-actions>
+              </v-card>
+            </v-item>
+          </v-col>
+        </v-row>
+        <!-- 카테고리 미 선택 시 전체 게시글 렌더링 -->
+        <v-row v-else>
           <v-col
             v-for="post in postList"
             :key="post.id"
@@ -90,14 +136,12 @@
 export default {
   name: 'PostItems',
   data: () => ({
-    tags: [
-      '학교',
-      '직장',
-      '대인 관계',
-      '가족',
-      '외모',
-      '등'
-    ]
+    category: ['우울', '불안', '성격문제', '애착', 
+          '대인기피', '분노조절', '자존감', 
+          '중독', '대인관계', '부부', '가족상담',
+        ],
+    selectCategories: [],
+    selectPosts: []
   }),
   methods: {
     showDetail: function (post) {
@@ -105,7 +149,24 @@ export default {
     },
     createPost: function () {
       this.$router.push({ name: 'CreatePost' })
-    }
+    },
+    // 선택한 카테고리에 해당하는 게시글 필터링
+    selectCategory: function (item) {
+      this.selectPosts = []
+      if (this.selectCategories.includes(item)){
+        const idx = this.selectCategories.indexOf(item)
+        this.selectCategories.splice(idx, 1)
+      } else {
+        this.selectCategories.push(item)
+      }      
+      for (const category of this.selectCategories) {
+        for (const post of this.$store.state.postlist) {
+            if (post.category === category){
+            this.selectPosts.push(post)
+          }
+        }
+      }
+    },
   },
   // 전체 게시글 호출 API 연결 store
   computed: {
