04954e7e96ae284c666b67f53f861d0663458c37
DESKTOP-KVCQHCD\multicampus <23gywls@naver.com>
2021-08-09 15:51:05 +0900

    #S05P13B208-120 feat: user_program 디비 설계

---
+++ backend/src/main/java/com/ssafy/db/repository/User_ProgramRepository.java
@@ -0,0 +1,10 @@
+package com.ssafy.db.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.db.entity.User_Program;
+
+public interface User_ProgramRepository extends JpaRepository<User_Program, Long>{
+
+}
+
