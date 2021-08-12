8d7a2d91d4c47ebe90c2c1916bb3d0982f862855
DESKTOP-KVCQHCD\multicampus <23gywls@naver.com>
2021-08-13 00:25:44 +0900

    userprogram 신청 오류

--- backend/src/main/java/com/ssafy/db/entity/User_Program.java
+++ backend/src/main/java/com/ssafy/db/entity/User_Program.java
@@ -1,5 +1,6 @@
 package com.ssafy.db.entity;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.Entity;
@@ -7,12 +8,14 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+//@Data
 public class User_Program extends BaseEntity {
 	//ManyToOne은 즉시 로딩이 기본값인데 연결된 엔티티 정보까지 한 번에 가져오려고 해서 성능에 문제가 발생할 수 있다.
 	@ManyToOne(fetch = FetchType.LAZY)
