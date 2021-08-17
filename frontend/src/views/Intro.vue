4a265e8cd0d99cc45421c632bd3309a274f5d934
오승철 <tmdcjf103@gmail.com>
2021-08-18 02:14:25 +0900

    fix: intro 스타일링 수정

--- frontend/src/views/Intro.vue
+++ frontend/src/views/Intro.vue
@@ -2,40 +2,46 @@
   <div>
     <section class="background">
       <div class="content-wrapper">
-        <p class="content-title">Better Mind</p>
-        <p class="content-subtitle">괜찮은 척 안해도 돼요.</p>
-        <p class="content-text">우리나라 국민 4명 중 1명은 평생에 한 번 이상 마음에 문제가 생겨요. </p>
-        <p class="content-text">하지만 관련 서비스를 받는 사람은 캐나다, 미국, 뉴질랜드의 반 정도밖에 되지 않죠.</p> 
-        <p class="content-text">내 마음이 건강하다고 생각하거나 스스로 해결할 수 있다고 생각하는 경우가 대부분이에요.</p> 
-        <p class="content-text">이제는 괜찮은 척하지 않아도 돼요. <strong>BetterMind</strong>가 도울 수 있어요.</p> 
+        <div class="glass">
+          <p class="content-title">Better Mind</p>
+          <p class="content-subtitle">괜찮은 척 안해도 돼요.</p>
+          <p class="content-text">우리나라 국민 4명 중 1명은 평생에 한 번 이상 마음에 문제가 생겨요. </p>
+          <p class="content-text">하지만 관련 서비스를 받는 사람은 캐나다, 미국, 뉴질랜드의 반 정도밖에 되지 않죠.</p> 
+          <p class="content-text">내 마음이 건강하다고 생각하거나 스스로 해결할 수 있다고 생각하는 경우가 대부분이에요.</p> 
+          <p class="content-text">이제는 괜찮은 척하지 않아도 돼요. <strong>BetterMind</strong>가 도울 수 있어요.</p> 
+        </div>
       </div>
     </section>
     <section class="background">
       <div class="content-wrapper">
-        <p class="content-title">화상 심리 상담 서비스</p>
-        <p class="content-subtitle">다양한 형태의 심리 상담을 제공해요.</p>
-        <p class="content-text">보다 전문적인 심리상담이 필요하나 고액의 상담비에 대한 부담감, 바쁜 생활 속에 상담센터를 직접 찾아가야 하는 어려움, </p>
-        <p class="content-text">주변의 시선에 대한 두려움 등을 보완할 수 있는 것이 필요하다는 생각에 이르렀습니다.</p>
-        <p class="content-text">그래서 부담이 적고 심리상담의 허들을 낮출 수 있는 서비스를 제공하려 합니다.</p>
-        <br>
-        <p class="content-text">화상 기술을 바탕으로 1:1 심리상담 "Mind One", 그룹 심리상담 "Mind Us",</p>
-        <p class="content-text">내담자 간에 같은 고민과 아픔을 공유하는 자조모임 "Mind With"이 탄생하였습니다.</p>
+        <div class="glass">
+          <p class="content-title">화상 심리 상담 서비스</p>
+          <p class="content-subtitle">다양한 형태의 심리 상담을 제공해요.</p>
+          <p class="content-text">보다 전문적인 심리상담이 필요하나 고액의 상담비에 대한 부담감, 바쁜 생활 속에 상담센터를 직접 찾아가야 하는 어려움, </p>
+          <p class="content-text">주변의 시선에 대한 두려움 등을 보완할 수 있는 것이 필요하다는 생각에 이르렀습니다.</p>
+          <p class="content-text">그래서 부담이 적고 심리상담의 허들을 낮출 수 있는 서비스를 제공하려 합니다.</p>
+          <br>
+          <p class="content-text">화상 기술을 바탕으로 1:1 심리상담 "Mind One", 그룹 심리상담 "Mind Us",</p>
+          <p class="content-text">내담자 간에 같은 고민과 아픔을 공유하는 자조모임 "Mind With"이 탄생하였습니다.</p>
+        </div>
       </div>
     </section>
     <section class="background">
       <div class="content-wrapper">
-        <p class="content-title">Better Touch</p>
-        <p class="content-subtitle">눈치 보지 않고 마음을 터놓고 위로와 응원을 받는 공간이에요.</p>
-        <p class="content-text">복잡하고 치열한 현대사회에서 너무도 많은 관계 속 상처를 받습니다.</p>
-        <p class="content-text">그러나 내 상처가 얼마나 깊은지, 어떻게 내 마음속 응어리를 덜어낼 수 있을지 알 수 없을 때가 많습니다.</p>
-        <p class="content-text">친구 또는 가족에게 털어놓기에는 상대의 시선이 부담스럽고, </p>
-        <p class="content-text">정신과 의사 혹은 심리상담사를 찾아 나서기에는 사회적 편견이 우리 발목을 붙잡습니다.</p>
-        <p class="content-text">여러분들이 더는 혼자 힘들어하지 않을 수 있도록 "익명 보장 마음 치유 서비스"를 제공합니다.</p>
+        <div class="glass">
+          <p class="content-title">Better Touch</p>
+          <p class="content-subtitle">눈치 보지 않고 마음을 터놓고 위로와 응원을 받는 공간이에요.</p>
+          <p class="content-text">복잡하고 치열한 현대사회에서 너무도 많은 관계 속 상처를 받습니다.</p>
+          <p class="content-text">그러나 내 상처가 얼마나 깊은지, 어떻게 내 마음속 응어리를 덜어낼 수 있을지 알 수 없을 때가 많습니다.</p>
+          <p class="content-text">친구 또는 가족에게 털어놓기에는 상대의 시선이 부담스럽고, </p>
+          <p class="content-text">정신과 의사 혹은 심리상담사를 찾아 나서기에는 사회적 편견이 우리 발목을 붙잡습니다.</p>
+          <p class="content-text">여러분들이 더는 혼자 힘들어하지 않을 수 있도록 "익명 보장 마음 치유 서비스"를 제공합니다.</p>
+        </div>
       </div>
       <div class="d-flex justify-center">
         <v-btn width="20vw" min-height="70px" @click="$router.push({ name: 'Login' })">
           <span class="text-h6">
-            BETTERMIND 방문하기
+            Visit BETTERMIND
           </span>
         </v-btn>
       </div>
