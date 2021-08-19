93cfacd61746c684fd50db2805fb0f95d7bdfd65
한건 <ofliberty@naver.com>
2021-08-19 22:59:54 +0900

    #S05P13B208-294 feat: webrtc.vue 자질구레 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,20 +14,20 @@ export default {
   name: 'App',
   components: {
   },
-  // created: function () {
-  //   if (this.$route.path !== '/') {
-  //     // 현재 사용자의 로그인 상태
-  //     const token = localStorage.getItem('jwt')
-  //     if (token) {
-  //       this.$store.commit('ON_LOGIN')     
-  //       this.$store.dispatch('getMyInfo')
-  //     }
-  //     // 로그아웃 상태이면 로그인 페이지로 이동
-  //     else {
-  //       this.$router.push({ name: 'Login' })
-  //     }
-  //   }
-  // }
+  created: function () {
+    if (this.$route.path !== '/') {
+      // 현재 사용자의 로그인 상태
+      const token = localStorage.getItem('jwt')
+      if (token) {
+        this.$store.commit('ON_LOGIN')     
+        this.$store.dispatch('getMyInfo')
+      }
+      // 로그아웃 상태이면 로그인 페이지로 이동
+      else {
+        this.$router.push({ name: 'Login' })
+      }
+    }
+  }
 };
 </script>
 <style>
